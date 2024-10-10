69139d4b30f96105962a5ecbe9e7cf320c37a2b3
Icaro Kiiler <icarokiilermelo@gmail.com>
2024-10-09 21:18:34 -0300

    feat: Files got moved to backend folder

--- README.md
+++
@@ -1,3 +0,0 @@
-# template-express
-This is a basic template of express api, with .env, jest, eslint and prettier.
-
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import express from 'express';
-
-const app = express();
-
-app.listen(process.env.APP_PORT || 5000, () => {
-  console.log(`Server started on port ${process.env.APP_PORT || 5000}!`);
-});
