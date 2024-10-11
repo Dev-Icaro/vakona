f4ecce8425ba823872aa992beaf5e1b1026ed9bb
Icaro-Softica <icarokiilermelo@gmail.com>
2024-10-11 09:46:04 -0300

    fix: Fix the problemn in the custom-init.sql

--- backend/db-init/custom-init.sql
+++ backend/db-init/custom-init.sql
@@ -9,11 +9,11 @@ CREATE TABLE app.users(
   phone_number VARCHAR(9),
   email VARCHAR(50) NOT NULL,
   cep VARCHAR(8),
-  "password" VARCHAR(50) NOT NULL,
+  "password" VARCHAR(100) NOT NULL,
   active_ind BOOLEAN DEFAULT TRUE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT NULL
 );
 
-INSERT INTO app.users("name", cpf, DDI, DDD, phone_number, email, cep, "password") 
-VALUES ('admin', '12345678901', '55', '11', '123456789', 'admin@admin.com.br', "$2b$10$Qcdjv2.iq2jhNm3LcafX/eXFAl3ecpgBom5yKdMBedJk0ZdA0LQqy");
+INSERT INTO app.users("name", cpf, phone_number, email, "password", cep) 
+VALUES ('admin', '12345678901', '123456789', 'admin@admin.com.br',  '$2b$10$Qcdjv2.iq2jhNm3LcafX/eXFAl3ecpgBom5yKdMBedJk0ZdA0LQqy', '00012345');
